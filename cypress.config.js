6099b5ab4d5dea83e7b1c3578c2404f025a1cb28
Rafael <rafaellorenzo.cc@gmail.com>
2026-09-16 01:45:24 -0300

    Added URL environment variable inside cypress.config.js and updated usage to Cypress.expose()

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    expose: {
+      URL: 'http://localhost:4000',
+    },
+    env: {
+
+    },
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
